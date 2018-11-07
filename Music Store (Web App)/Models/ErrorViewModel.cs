55b3b48dd6b47c6635f0bfe49efdfe24ff2d4d41
Michael Stevens - Allphi <michael.stevens@allphi.eu>
2018-11-07 10:53:11 +0100

    Add project files.

---
+++ Music Store (Web App)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Music_Store__Web_App_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
